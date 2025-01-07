647ad5c2d6f06ae36e0b63f402bc1e32e2dd30fa
Jimena García <125323742+jimena-garcia@users.noreply.github.com>
2025-01-06 22:53:51 -0600

    Update sobre.js

--- sobre.js
+++ sobre.js
@@ -9,6 +9,6 @@ function redirectToIndex() {
    
    // Espera a que termine la animación y luego redirige
    setTimeout(function () {
-     window.location.href = '/Index.html';
+     window.location.href = 'Index.html';
    }, 1000); // Asegúrate de que esto coincide con la duración de la animación fade-out
 }
